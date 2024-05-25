ca2a567b3290ab2b3b4b84be51ec7ad48e0fb288
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-05-25 09:17:21 +0800

    Add files via upload

---
+++ lab js 1/main.js
@@ -0,0 +1,28 @@
+
+
+//buat variabel let nama, asal daerah, tgl.lahir, umur, pekerjaan
+
+// let nama = "Bpk.Hamdan Al Fattah";
+// let Asal = "Bone - Indonesia";
+// let TanggalLahir = "19 Mei 2004 - 2024";
+// let Pekerjaan = "mahasiswa"
+
+// console.log(`nama: ${nama} 
+// Asal: ${Asal}
+// Tempat tanggal lahir: ${TanggalLahir} 
+// Pekerjaan: ${Pekerjaan}`
+// );
+
+//
+
+
+let nama = "Bpk.Hamdan Al Fattah";
+let Asal = "Bone - Indonesia";
+let TahunLahir = "2024 - 2004";
+
+
+console.log(`nama: ${nama} 
+Asal: ${Asal}
+Tempat tanggal lahir: ${TahunLahir} 
+`
+);
